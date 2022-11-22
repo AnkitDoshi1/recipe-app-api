c6189d4e16175e4d2fd7e936c43a0706001a0a4e
AnkitDoshi1 <117649190+aaddemo@users.noreply.github.com>
2022-11-22 11:47:44 +0530

    New Commit

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add x and y return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subract x and y return result."""
+    return y-x
+
